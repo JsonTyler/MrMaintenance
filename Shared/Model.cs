8910d65ec8365912cd8f47b6c84422b6c1ca6457
JsonTyler <jasontylerrodriguez@gmail.com>
2021-12-25 17:25:01 -0800

    Restructures application and breaks into components, removes unneeded css files

---
+++ Shared/Model.cs
@@ -0,0 +1,8 @@
+    // The Model class
+    public class Model
+    {
+        public string Classes { get; set; } = "";
+        public int Order { get; set; }
+        public string Name { get; set; } = "";
+        public bool IsDragOver { get; set; }
+    }
